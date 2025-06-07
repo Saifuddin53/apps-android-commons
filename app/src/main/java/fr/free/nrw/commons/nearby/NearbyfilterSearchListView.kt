f1747ab6cce1e76d56109b501b92d71a8227421f
Saifuddin Adenwala <adenwala32@gmail.com>
2025-06-07 10:46:03 +0530

    Convert fragments and utils to Kotlin

---
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyfilterSearchListView.kt
@@ -0,0 +1,11 @@
+package fr.free.nrw.commons.nearby
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ListView
+
+class NearbyfilterSearchListView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ListView(context, attrs, defStyleAttr)
